8305c88db285bcd47a2460d6b15df7f3e4bf70c3
Vasileios Charatsidis <charatsidisvasileios@gmail.com>
2018-05-24 17:50:35 +0200

    creation of core tested

--- Main.cpp
+++ Main.cpp
@@ -3,21 +3,45 @@
 #include "Solver.cpp"
 using namespace std;
 
+void print_neighbours(Graph* graph)
+{
+	int size = graph->nodes.size();
+	int number_edges = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		int node_id = graph->nodes[i]->id;
+
+		std::vector<int> neighbours = graph->nodes[i]->neighbours;
+		int neighbours_size = neighbours.size();
+
+		std::cout << "neighbours of " + std::to_string(node_id) << std::endl;
+
+		for (int j = 0; j < neighbours_size; j++)
+		{
+			std::cout << "node id " + std::to_string(neighbours[j]) + ", ";
+			number_edges++;
+		}
+
+		std::cout << " " << std::endl;
+	}
+	std::cout << "number of edges = " + std::to_string(number_edges / 2) << std::endl;
+}
 int main() {
 	
 	//Test GraphMaker nodes
 	GraphMaker* gr = new GraphMaker();
 
-	gr->read("1000_9");
+	gr->read("1000_5");
 	Graph* graph = gr->graph;
 
 	int graph_size = graph->nodes.size();
 
-	for (int i = 0; i < graph_size; i++)
+	/*for (int i = 0; i < graph_size; i++)
 	{
 		int node_id = graph->nodes[i]->id;
-		//std::cout << "node id " + std::to_string(node_id) << std::endl;
-	}
+		std::cout << "node id " + std::to_string(node_id) << std::endl;
+	}*/
 
 	//Test GraphMaker edges
 	/*int number_edges = 0;
@@ -54,5 +78,84 @@ int main() {
 		
 	}*/
 
+	//Test solver core
+	Solver* solver = new Solver(graph);
+	solver->solve();
+
+	print_neighbours(solver->core);
+
+	//Test core 2
+	/*Node* n0 = new Node();
+	n0->id = 0;
+	n0->neighbours.push_back(1);
+	n0->neighbours.push_back(8);
+	
+	Node* n1 = new Node();
+	n1->id = 1;
+	n1->neighbours.push_back(0);
+	n1->neighbours.push_back(2);
+	n1->neighbours.push_back(8);
+
+	Node* n2 = new Node();
+	n2->id = 2;
+	n2->neighbours.push_back(1);
+	n2->neighbours.push_back(3);
+	n2->neighbours.push_back(5);
+	n2->neighbours.push_back(4);
+
+	Node* n3 = new Node();
+	n3->id = 3;
+	n3->neighbours.push_back(2);
+	n3->neighbours.push_back(5);
+	n3->neighbours.push_back(4);
+
+	Node* n4 = new Node();
+	n4->id = 4;
+	n4->neighbours.push_back(3);
+	n4->neighbours.push_back(2);
+	n4->neighbours.push_back(5);
+	n4->neighbours.push_back(8);
+
+	Node* n5 = new Node();
+	n5->id = 5;
+	n5->neighbours.push_back(2);
+	n5->neighbours.push_back(3);
+	n5->neighbours.push_back(4);
+	n5->neighbours.push_back(8);
+
+	Node* n6 = new Node();
+	n6->id = 6;
+	n6->neighbours.push_back(7);
+	n6->neighbours.push_back(8);
+
+	Node*n7 = new Node();
+	n7->id = 7;
+	n7->neighbours.push_back(6);
+	n7->neighbours.push_back(8);
+
+	Node* n8 = new Node();
+	n8->id = 8;
+	n8->neighbours.push_back(6);
+	n8->neighbours.push_back(7);
+	n8->neighbours.push_back(0);
+	n8->neighbours.push_back(1);
+	n8->neighbours.push_back(5);
+	n8->neighbours.push_back(4);
+
+	Graph* test = new Graph();
+	test->nodes.push_back(n0);
+	test->nodes.push_back(n1);
+	test->nodes.push_back(n2);
+	test->nodes.push_back(n3);
+	test->nodes.push_back(n4);
+	test->nodes.push_back(n5);
+	test->nodes.push_back(n6);
+	test->nodes.push_back(n7);
+	test->nodes.push_back(n8);
+
+	Solver* test_solver = new Solver(test);
+	test_solver->solve();
+	print_neighbours(test_solver->core);*/
+	
 	return 0;
 }
--- Solver.cpp
+++ Solver.cpp
@@ -1,17 +1,23 @@
 #include "Graph.cpp";
+#include <iostream>
+#include <string>
 using std::vector;
 
 class Solver {
 public:
 	Graph* graph;
 	Solver(Graph* give_graph) : graph(give_graph) {};
+	Graph* core;
 
 	void solve() {
+		core = graph->clone();
 		int size = graph->nodes.size();
-		for (int number_colors = 2; number_colors < size; number_colors++)
-		{
-			setup_colors(40);
-		}
+
+		//for (int number_colors = 2; number_colors < size; number_colors++)
+		//{
+			setup_colors(100);
+			delete_unimportant_nodes(100);
+		//}
 	}
 
 private:
@@ -25,7 +31,55 @@ private:
 			for (int color = 0; color < number_colors; color++)
 			{
 				graph->nodes[node]->available_colors.push_back(1);
+				core->nodes[node]->available_colors.push_back(1);
 			}
+		}
+	}
+
+	void delete_unimportant_nodes(int number_colors)
+	{
+		int deleted = 0;
+		int temp = -1;
+
+		while (temp != deleted) 
+		{
+			int size = core->nodes.size();
+			temp = deleted;
+			for (int node = 0; node < size; node++)
+			{
+				int neighbours = core->nodes[node]->neighbours.size();
+
+				if (neighbours < number_colors - 1)
+				{
+					
+					int node_id = core->nodes[node]->id;
+					std::cout << "node to be deleted " + std::to_string(node_id)+" deleted = "+std::to_string(deleted) << std::endl;
+
+					update_core_neighbours(node_id);
+					core->nodes.erase(core->nodes.begin() + node);
+					deleted++;
+					break;
+				}
+			}
+		}
+	}
+
+	void update_core_neighbours(int to_be_deleted)
+	{
+		int size = core->nodes.size();
+
+		for (int node = 0; node < size; node++)
+		{
+			int neighbours_size = core->nodes[node]->neighbours.size();
+
+			for (int j = 0; j < neighbours_size; j++)
+			{
+				if (core->nodes[node]->neighbours[j] == to_be_deleted)
+				{
+					core->nodes[node]->neighbours.erase(core->nodes[node]->neighbours.begin() + j);
+					break;
+				}
+			}	
 		}
 	}
 };
