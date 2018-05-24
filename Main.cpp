534c48b27ac459c14172c06dcbc750a683d3a3b6
Vasileios Charatsidis <charatsidisvasileios@gmail.com>
2018-05-24 13:57:27 +0200

    GraphMaker done and tested

--- Main.cpp
+++ Main.cpp
@@ -1,19 +1,41 @@
 // reading a text file
-#include "GraphReader.cpp"
+#include "GraphMaker.cpp"
 using namespace std;
 
 int main() {
 	
-	//Test GraphReader
-	GraphReader* gr = new GraphReader();
+	//Test GraphReader nodes
+	GraphMaker* gr = new GraphMaker();
 
-	Graph* graph = gr->read("4_1");
+	gr->read("1000_9");
+	Graph* graph = gr->graph;
 
-	for (int i = 0; i < graph->nodes.size(); i++)
+	int graph_size = graph->nodes.size();
+
+	for (int i = 0; i < graph_size; i++)
 	{
 		int node_id = graph->nodes[i]->id;
-		std::cout << "node id " + std::to_string(node_id) << std::endl;
+		//std::cout << "node id " + std::to_string(node_id) << std::endl;
 	}
+
+	//Test GraphReader edges
+	int number_edges = 0;
+	for (int i = 0; i < graph_size; i++)
+	{
+		int node_id = graph->nodes[i]->id;
+		std::vector<int> neighbours = graph->nodes[node_id]->neighbours;
+		int neighbours_size = neighbours.size();
+		std::cout << "neighbours of " + std::to_string(node_id) << std::endl;
+
+		for (int j = 0; j < neighbours_size; j++)
+		{
+			std::cout << "node id " + std::to_string(neighbours[j]) + ", ";
+			number_edges++;
+		}
+
+		std::cout << " " << std::endl;
+	}
+	std::cout << "number of edges = "+std::to_string(number_edges/2) << std::endl;
 
 	return 0;
 }
--- Node.cpp
+++ Node.cpp
@@ -2,6 +2,6 @@
 
 struct Node {
 	int id;
-	std::vector<int> neightbours;
+	std::vector<int> neighbours;
 	std::vector<int> available_colors;
 };
